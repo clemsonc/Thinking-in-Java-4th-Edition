28ebba692349b0250f17d06a0da34e96e03ce5fe
Clemson Chan <clemsonc@yahoo.com>
2015-03-13 15:22:40 -0700

    chapter 2 exercise 1 initial check in

---
+++ src/Chapter2/Exercise1.java
@@ -0,0 +1,11 @@
+package Chapter2;
+
+public class Exercise1 {
+    static public int i;
+    static public String ch;
+    
+    public static void main(String[] args) {
+    	System.out.println("the initial value of an integer is: "+i);
+    	System.out.println("the initial value of a string is: "+ch);
+    }
+}
