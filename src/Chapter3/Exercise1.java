ee06cc08cd8d10e4f8c4b776a798c5dd687c0f6f
Clemson Chan <clemsonc@yahoo.com>
2015-03-13 15:10:26 -0700

    Basic print function

---
+++ src/Chapter3/Exercise1.java
@@ -0,0 +1,10 @@
+public class Exercise1 {	
+	private static void cprint(String string) {
+		System.out.println(string);		
+	} 
+	public static void main(String[] args) {
+		Integer value = 100;
+		cprint(value + " is an integer value, using the short way to print.");
+		System.out.println(value + " is an integer value, using System.out to print.");
+	}
+}
